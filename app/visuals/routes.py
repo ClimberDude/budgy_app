2a90e064ef62dc13fa2aacaee759c37e02ed06de
ClimberDude <33363057+ClimberDude@users.noreply.github.com>
2019-12-07 17:03:59 -0800

    Visualization fix
    redirect to landing page if vis page errors out

--- app/visuals/routes.py
+++ app/visuals/routes.py
@@ -26,6 +26,7 @@ def income_v_spending():
         data = da.income_v_spending_plot()
     except:
         flash('There are currently no transaction. Add some to see visualizations!')
+        return redirect(url_for('main.landing'))
 
     budget_categories = [(c.id,c.category_title) for c in current_user.budget_categories.filter_by(status='A').order_by(Budget_Category.category_title.asc()).all()]
     spending_categories = [(c.spending_category,c.spending_category) for c in Budget_Category.query.with_entities(Budget_Category.spending_category).distinct()]
@@ -44,7 +45,7 @@ def income_v_spending():
                                 )
         except:
             flash('There are currently no transaction. Add some to see visualizations!')
-
+            return redirect(url_for('main.landing'))
 
         return render_template('income_v_spending.html',
                             title='Income vs Spending',
@@ -59,17 +60,23 @@ def income_v_spending():
 @bp.route('/spending_by_category', methods=['GET', 'POST'])
 @login_required
 def spending_by_category():
-
-    data = da.spending_by_category_plot()
+    try:
+        data = da.spending_by_category_plot()
+    except:
+        flash('There are currently no transaction. Add some to see visualizations!')
+        return redirect(url_for('main.landing'))
 
     form = SpendingByCategoryVisForm()
 
     if form.validate_on_submit():
-
-        data = da.spending_by_category_plot(start_date=form.start_date.data,
-                            end_date=form.end_date.data,
-                            budget_or_spending=form.budget_or_spending.data
-                            )
+        try:
+            data = da.spending_by_category_plot(start_date=form.start_date.data,
+                                end_date=form.end_date.data,
+                                budget_or_spending=form.budget_or_spending.data
+                                )
+        except:
+            flash('There are currently no transaction. Add some to see visualizations!')
+            return redirect(url_for('main.landing'))
 
         return render_template('spending_by_category.html',
                             title='Income vs Spending',
